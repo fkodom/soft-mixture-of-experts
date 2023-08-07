b2f72a8b5a965235f3a4fc1792970137f3bfc6e1
Frank Odom <frank.odom.iii@mgmail.com>
2023-08-07 16:24:25 -0500

    Initial SoftMoE layer; update README

--- README.md
+++ README.md
@@ -1,35 +1,92 @@
 # soft-mixture-of-experts
 
+> work in progress
+
+PyTorch implementation of Soft MoE by Google Brain in [From Sparse to Soft Mixtures of Experts](https://arxiv.org/abs/2308.00951.pdf)
+
+<img src="doc/soft-moe-layer.jpeg" alt="soft-moe-layer" width="600"/>
+
+### TODO
+
+- [x] Implement Soft MoE layer
+- [ ] Set up unit tests (convert from debug code 😅)
+- [ ] Example end-to-end Transformer models
+    - [ ] language model
+    - [ ] vision transformer
+- [ ] Reproduce benchmarks from paper
+    - [ ] ViT eval step (Tables 1,2)
+    - [ ] ViT training step ()
+- [ ] Release on PyPI
+    - [ ] Prerelease
+    - [ ] Stable
+
 
 ## Install
 
+PyPI:
+> work in progress
+
+From source:
 ```bash
 pip install "soft-mixture-of-experts @ git+ssh://git@github.com/fkodom/soft-mixture-of-experts.git"
+```
 
-# Install all dev dependencies (tests etc.)
-pip install "soft-mixture-of-experts[test] @ git+ssh://git@github.com/fkodom/soft-mixture-of-experts.git"
-
+For contributors:
+```bash
+# Clone/fork this repo
+gh repo clone fkodom/soft-mixture-of-experts
+cd soft-mixture-of-experts
+# Install all dev dependencies (tests etc.) in editable mode
+pip install -e .[test]
 # Setup pre-commit hooks
 pre-commit install
 ```
 
 
+## Usage
+
+### `SoftMoE`
+
+```python
+import torch
+
+from soft_mixture_of_experts.soft_moe import SoftMoE
+
+device = "cuda"
+# SoftMoE with 32 experts, 2 slots per expert (64 total):
+moe = SoftMoE(
+    embed_dim=512,
+    num_experts=32,
+    slots_per_expert=2,
+    bias=False,  # optional, default: True
+)
+# input shape: (batch_size, seq_len, embed_dim)
+x = torch.randn(2, 128, 512, device="cuda")
+y = moe(x)
+print(y.shape)
+# torch.Size([2, 128, 512])
+```
+
+
 ## Test
 
-Tests run automatically through GitHub Actions.
-* Fast tests run on each push.
-* Slow tests (decorated with `@pytest.mark.slow`) run on each PR.
+Tests run automatically through GitHub Actions on each `git push`.
 
 You can also run tests manually with `pytest`:
 ```bash
 pytest
-# For all tests, including slow ones:
-pytest --slow
 ```
 
 
-## Release (Optional)
+## Citations
 
-**NOTE**: Requires `publish.yaml` workflow to be enabled., and a valid PyPI token to be set as `PYPI_API_TOKEN` in the repo secrets.
-
-Tag a new release in this repo, and GHA will automatically publish Python wheels.
+```bibtex
+@misc{puigcerver2023sparse,
+      title={From Sparse to Soft Mixtures of Experts}, 
+      author={Joan Puigcerver and Carlos Riquelme and Basil Mustafa and Neil Houlsby},
+      year={2023},
+      eprint={2308.00951},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+```
---
+++ soft_mixture_of_experts/soft_moe.py
@@ -0,0 +1,217 @@
+from __future__ import annotations
+
+import math
+from typing import Callable, Optional, Sequence, Union
+
+import torch
+import torch.nn.functional as F
+from einops import einsum, rearrange
+from torch import Tensor, nn
+
+ExpertFunction = Callable[[Tensor], Tensor]
+
+
+class MultiExpertLayer(nn.Module):
+    """A more efficient alternative to creating 'n' separate expert layers (possibly
+    from 'nn.Linear' modules).  Instead, we create a single set of batched weights
+    and biases, and all 'experts' in parallel.
+
+    Args:
+        embed_dim (int): embedding dimension (d)
+        num_experts (int): number of experts (n)
+        bias (bool): whether to include a bias term. Default: True
+    """
+
+    def __init__(
+        self,
+        embed_dim: int,
+        num_experts: int,
+        bias: bool = True,
+        device: Optional[Union[torch.device, str]] = None,
+        dtype: Optional[torch.dtype] = None,
+    ):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_experts = num_experts
+
+        self.weight = nn.Parameter(
+            torch.empty((num_experts, embed_dim, embed_dim), device=device, dtype=dtype)
+        )
+        bias_param: Optional[nn.Parameter] = None
+        if bias:
+            bias_param = nn.Parameter(
+                torch.empty((num_experts, embed_dim), device=device, dtype=dtype)
+            )
+        # Include type annotation for mypy :D
+        self.bias: Optional[nn.Parameter]
+        self.register_parameter("bias", bias_param)
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # NOTE: Mostly copy-pasta from 'nn.Linear.reset_parameters'
+        #
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = einsum(x, self.weight, "b n ... d1, n d1 d2 -> b n ... d2")
+        if self.bias is not None:
+            if x.ndim == 3:
+                bias = rearrange(self.bias, "n d -> () n d")
+            elif x.ndim == 4:
+                bias = rearrange(self.bias, "n d -> () n () d")
+            else:
+                raise ValueError(
+                    f"Expected input to have 3 or 4 dimensions, but got {x.ndim}"
+                )
+            x = x + bias
+        return x
+
+    def extra_repr(self) -> str:
+        return (
+            f"embed_dim={self.embed_dim}, num_experts={self.num_experts}, "
+            f"bias={self.bias is not None}"
+        )
+
+
+class SoftMoE(nn.Module):
+    """A PyTorch module for Soft-MoE, as described in the paper:
+        "From Sparse to Soft Mixtures of Experts"
+        https://arxiv.org/pdf/2308.00951.pdf
+
+    einstein notation:
+    - b: batch size
+    - m: input sequence length
+    - d: embedding dimension
+    - n: num experts
+    - p: num slots per expert
+    - (n * p): total number of slots
+
+    Args:
+        embed_dim (int): embedding dimension (d)
+        num_experts (int): number of experts (n)
+        slots_per_expert (int): number of slots per expert (p)
+        bias (bool): whether to include a bias term. Default: True.
+    """
+
+    def __init__(
+        self,
+        embed_dim: int,
+        num_experts: int,
+        slots_per_expert: int,
+        bias: bool = True,
+        device: Optional[Union[str, torch.device]] = None,
+        dtype: Optional[torch.dtype] = None,
+    ):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_experts = num_experts
+        self.slots_per_expert = slots_per_expert
+        self.bias = bias
+
+        # TODO: Check for initialization strategy from the paper
+        self.phi = nn.Parameter(
+            torch.empty(
+                (embed_dim, num_experts, slots_per_expert),
+                device=device,
+                dtype=dtype,
+            )
+        )
+        self.experts = MultiExpertLayer(
+            embed_dim=embed_dim,
+            num_experts=num_experts,
+            bias=bias,
+            device=device,
+            dtype=dtype,
+        )
+
+    def reset_parameters(self) -> None:
+        # NOTE: Copy weight initialization from 'nn.Linear.reset_parameters'
+        # TODO: Check for initialization strategy from the paper
+        nn.init.kaiming_uniform_(self.phi, a=math.sqrt(5))
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Forward pass for the Soft-MoE layer, as described in:
+            https://arxiv.org/pdf/2308.00951.pdf
+        See: equations (1-3), algorithm 1, and figure 2
+
+        einstein notation:
+        - b: batch size
+        - m: input sequence length
+        - d: embedding dimension
+        - n: num experts
+        - p: num slots per expert
+        - (n * p): total number of slots
+
+        Args:
+            x (Tensor): input tensor of shape (b, m, d)
+
+        Returns:
+            Tensor: output tensor of shape (b, m, d)
+        """
+        if x.size(-1) != self.embed_dim:
+            raise ValueError(
+                f"Expected x.size(-1)={x.size(-1)} to match embed_dim={self.embed_dim}, "
+                f"but got {x.size(-1)}."
+            )
+
+        logits = einsum(x, self.phi, "b m d, d n p -> b m n p")
+        dispatch_weights = logits.softmax(dim=0)  # denoted 'D' in the paper
+        # NOTE: The 'torch.softmax' function does not support multiple values for the
+        # 'dim' argument (unlike jax), so we are forced to flatten the last two dimensions.
+        # Then, we rearrange the Tensor into its original shape.
+        combine_weights = rearrange(
+            logits.flatten(start_dim=2).softmax(dim=-1),
+            "b m (n p) -> b m n p",
+            n=num_experts,
+        )
+
+        # NOTE: To save memory, I don't rename the intermediate tensors Y, Ys, Xs.
+        # Instead, I just overwrite the 'x' variable.  The names from the paper are
+        # included in a comment for each line below.
+        x = einsum(x, dispatch_weights, "b m d, b m n p -> b n p d")  # Xs
+        x = self.experts(x)  # Ys
+        x = einsum(x, combine_weights, "b n p d, b m n p -> b m d")  # Y
+
+        return x
+
+
+if __name__ == "__main__":
+    # testing/debugging
+    # TODO: convert to unit tests
+
+    batch_size = 2
+    seq_len = 16
+    embed_dim = 2048
+    num_experts = 32
+    slots_per_expert = 2
+
+    device = "cuda"
+    dtype = torch.float16
+
+    x = torch.randn((batch_size, seq_len, embed_dim), device=device, dtype=dtype)
+    # phi = torch.randn(
+    #     embed_dim, num_experts, slots_per_expert, device=device, dtype=dtype
+    # )
+    # experts = [
+    #     torch.nn.Linear(embed_dim, embed_dim, device=device, dtype=dtype)
+    #     for _ in range(num_experts)
+    # ]
+    # multi_expert = MultiExpertLayer(embed_dim, num_experts, device=device, dtype=dtype)
+    moe = SoftMoE(
+        embed_dim=embed_dim,
+        num_experts=num_experts,
+        slots_per_expert=slots_per_expert,
+        device=device,
+        dtype=dtype,
+    )
+    linear = nn.Linear(embed_dim, embed_dim, device=device, dtype=dtype)
+    y = moe.forward(x)
+    print(x.shape, y.shape)
